117cbd3e7313420e0a2285455f05bb07aae14f57
Joe Thorngren <joethorngren@gmail.com>
2017-05-13 17:09:18 -0600

    Initial commit.

---
+++ main/src/init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get upgrade
+
+# Add Repos
+
+sudo add-apt-repository ppa:git-core/ppa
+
+# Install Git
+
+sudo apt-get install git
+
+git config --global user.name "Joe Thorngren"
+git config --global user.email "joethorngren@gmail.com"
+
+# i3
+
+/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2017.01.02_all.deb keyring.deb SHA256:4c3c6685b1181d83efe3a479c5ae38a2a44e23add55e16a328b8c8560bf05e5f
+apt install ./keyring.deb
+echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
+apt update
+apt install i3
