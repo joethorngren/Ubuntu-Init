122c0abb687bfe9e31c00724833935757753a3af
Joe Thorngren <joethorngren@gmail.com>
2017-05-15 14:06:01 -0600

    Move i3 stuff together.

--- main/src/start.sh
+++ main/src/start.sh
@@ -32,14 +32,17 @@ echo "Done upgrading!"
 echo ""
 echo ""
 
+sudo apt-get autoremove
+sudo apt-get autoclean
+
 # Add Git Repository + Install Git
 
 rm -rf Templates Public Music Videos Examples Pictures examples.desktop
 mkdir ~/Apps ~/Android ~/Code_Complete/
 
-sudo add-apt-repository ppa:git-core/ppa -y
-sudo add-apt-repository ppa:webupd8team/java -y
-sudo apt-add-repository -y ppa:teejee2008/ppa -y
+sudo add-apt-repository -y ppa:git-core/ppa
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-add-repository -y ppa:teejee2008/ppa
 
 sudo apt-get update -y
 
@@ -64,7 +67,7 @@ sudo dpkg -i ~/Downloads/slack-desktop-2.6.0-amd64.deb
 
 # Install Calibre
 
-wget -nv -O- https://download.calibre-ebook.com/linux-installer.py | python -c "import sys; main=lambda x,y:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main('~/Apps/Calibre', True)"
+wget -nv -O- https://download.calibre-ebook.com/linux-installer.py | python -c "import sys; main=lambda x,y:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main('~/Apps/', True)"
 
 # Z-Shell
 
@@ -75,5 +78,5 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 /usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2017.01.02_all.deb keyring.deb SHA256:4c3c6685b1181d83efe3a479c5ae38a2a44e23add55e16a328b8c8560bf05e5f
 sudo apt install ./keyring.deb
 echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
-sudo apt update
-sudo apt install i3 -y
+sudo apt update -y
+sudo apt install -y -f i3 i3blocks i3status i3lock
