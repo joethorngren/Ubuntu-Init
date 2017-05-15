fdbcccd4134ffc310907549bd3951d239e7567ac
Joe Thorngren <joethorngren@gmail.com>
2017-05-15 13:53:24 -0600

    More stuff.

--- main/src/start.sh
+++ main/src/start.sh
@@ -24,7 +24,7 @@ echo "***********"
 echo ""
 echo ""
 
-sudo apt-get upgrade -y
+sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
 
 echo ""
 echo ""
@@ -34,26 +34,16 @@ echo ""
 
 # Add Git Repository + Install Git
 
-rm -rf Templates Public Music Videos Pictures examples.desktop
-mkdir ~/Apps ~/Android
+rm -rf Templates Public Music Videos Examples Pictures examples.desktop
+mkdir ~/Apps ~/Android ~/Code_Complete/
 
 sudo add-apt-repository ppa:git-core/ppa -y
 sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-add-repository -y ppa:teejee2008/ppa -y
 
+sudo apt-get update -y
 
-sudo apt-get install -y arandr pavucontrol unzip vim git git-core chromium-browser fonts-powerline
-
-# Z-Shell
-
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# i3
-
-/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2017.01.02_all.deb keyring.deb SHA256:4c3c6685b1181d83efe3a479c5ae38a2a44e23add55e16a328b8c8560bf05e5f
-sudo apt install ./keyring.deb
-echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
-sudo apt update
-sudo apt install i3 -y
+sudo apt-get install -y arandr pavucontrol unzip thunar vim git git-core chromium-browser timeshift fonts-powerline zsh zsh-completions oracle-java8-installer
 
 # IntelliJ
 
@@ -75,3 +65,15 @@ sudo dpkg -i ~/Downloads/slack-desktop-2.6.0-amd64.deb
 # Install Calibre
 
 wget -nv -O- https://download.calibre-ebook.com/linux-installer.py | python -c "import sys; main=lambda x,y:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main('~/Apps/Calibre', True)"
+
+# Z-Shell
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# i3
+
+/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2017.01.02_all.deb keyring.deb SHA256:4c3c6685b1181d83efe3a479c5ae38a2a44e23add55e16a328b8c8560bf05e5f
+sudo apt install ./keyring.deb
+echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
+sudo apt update
+sudo apt install i3 -y
