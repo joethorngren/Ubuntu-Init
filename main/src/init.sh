8aa21a7c550a7f0451f5e79cf7e83bcb00e0342f
Joe Thorngren <joethorngren@gmail.com>
2017-05-15 10:32:43 -0600

    Move Git install to init.sh

--- main/src/init.sh
+++ main/src/init.sh
@@ -2,6 +2,13 @@
 
 sudo apt-get update && sudo apt-get upgrade
 
+# Install Git
+
+sudo apt-get install git
+
+git config --global user.name "Joe Thorngren"
+git config --global user.email "joethorngren@gmail.com"
+
 git clone https://github.com/joethorngren/Ubuntu-Init.git
 cd Ubuntu-Init/
 chmod +x start.sh
--- main/src/start.sh
+++ main/src/start.sh
@@ -10,13 +10,6 @@ sudo add-apt-repository ppa:git-core/ppa
 
 sudo apt-get install vim
 
-# Install Git
-
-sudo apt-get install git
-
-git config --global user.name "Joe Thorngren"
-git config --global user.email "joethorngren@gmail.com"
-
 # i3
 
 /usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2017.01.02_all.deb keyring.deb SHA256:4c3c6685b1181d83efe3a479c5ae38a2a44e23add55e16a328b8c8560bf05e5f
