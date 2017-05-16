f91ce72b56acba3d645d7b3e965ac2ab8f16f52d
Joe Thorngren <joethorngren@gmail.com>
2017-05-15 22:36:32 -0600

    Erry'thang.

---
+++ main/src/init-android.sh
@@ -0,0 +1,5 @@
+# Android SDK
+
+# wget -O ~/Downloads/tools_r25.2.3-linux.zip https://dl.google.com/android/repository/tools_r25.2.3-linux.zip
+# unzip tools_r25.2.3-linux.zip -d ~/Android/
+
--- main/src/start.sh
+++ main/src/start.sh
@@ -88,7 +88,7 @@ sudo apt-get update -y
 
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 
-sudo apt-get install -y oracle-java8-installer arandr pavucontrol unzip thunar htop vim git git-core chromium-browser  fonts-powerline zsh zsh-completions 
+sudo apt-get install -y oracle-java8-installer curl arandr pavucontrol unzip thunar htop vim git git-core chromium-browser  fonts-powerline zsh zsh-completions 
 
 # timeshift
 
@@ -101,4 +101,32 @@ echo ""
 git config --global user.email "joethorngren@gmail.com"
 git config --global user.name "Joe Thorngren"
 
+wget -O ~/Downloads/ideaIU-2017.1.2.tar.gz https://download.jetbrains.com/idea/ideaIU-2017.1.2.tar.gz
+tar -zxvf ~/Downloads/ideaIU-2017.1.2.tar.gz -C ~/Apps/
+cp ././lib/intellij-settings.jar ~/intellij-settings.jar
+cp ././lib/studio-settings.jar ~/studio-settings.jar
+
+# Install Slack
+
+wget -O ~/Downloads/slack-desktop-2.6.0-amd64.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-2.6.0-amd64.deb
+sudo dpkg -i ~/Downloads/slack-desktop-2.6.0-amd64.deb
+sudo apt-get -fy install
+
+# Install Calibre
+
+wget -nv -O- https://download.calibre-ebook.com/linux-installer.py | python -c "import sys; main=lambda x,y:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main('~/Apps/', True)"
+
+# Z-Shell
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+## i3
+
+/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2017.01.02_all.deb keyring.deb SHA256:4c3c6685b1181d83efe3a479c5ae38a2a44e23add55e16a328b8c8560bf05e5f
+sudo apt install ./keyring.deb
+echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
+sudo apt update -y
+sudo apt install -y -f i3 i3blocks i3status i3lock
+
+reboot
 
