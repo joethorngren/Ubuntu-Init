9429bfbec46f80fe5e1f5c2c56d947684c294fa3
Joe Thorngren <joethorngren@gmail.com>
2017-05-15 21:46:00 -0600

    Condensing start scripts. start-1.sh just ran great on LH after clean install. Need to fix prompts during Java install.

--- main/src/start-1.sh
+++ main/src/start-1.sh
@@ -49,4 +49,80 @@ echo "Done cleaning!"
 echo ""
 echo ""
 
-# reboot
+echo ""
+echo ""
+echo "***********"
+echo "Initializing file system..."
+echo "***********"
+echo ""
+echo ""
+
+
+rm -rf Templates Public Music Videos Examples Pictures examples.desktop
+mkdir ~/Apps ~/Android ~/Code_Complete/
+
+echo ""
+echo ""
+echo "File system initialized!"
+echo ""
+echo ""
+
+echo ""
+echo ""
+echo "***********"
+echo "Adding repositories..."
+echo "***********"
+echo ""
+echo ""
+
+
+# zsh-completions
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-completions/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/zsh-completions.list"
+
+# Add zsh-completions repo key to apt
+wget -nv http://download.opensuse.org/repositories/shells:zsh-users:zsh-completions/xUbuntu_16.04/Release.key -O Release.key
+sudo apt-key add - < Release.key
+
+sudo add-apt-repository -y ppa:git-core/ppa
+sudo add-apt-repository -y ppa:webupd8team/java
+# sudo apt-add-repository -y ppa:teejee2008/ppa
+
+echo ""
+echo ""
+echo "Repositories added!"
+echo ""
+echo ""
+
+echo ""
+echo ""
+echo "***********"
+echo "Updating..."
+echo "***********"
+echo ""
+echo ""
+
+sudo apt-get update -y
+
+echo ""
+echo ""
+echo "Done updating!"
+echo ""
+echo ""
+
+echo ""
+echo ""
+echo "***********"
+echo "Installing Software..."
+echo "***********"
+echo ""
+echo ""
+
+sudo apt-get install -y arandr pavucontrol unzip thunar htop vim git git-core chromium-browser  fonts-powerline zsh zsh-completions oracle-java8-installer
+
+# timeshift
+
+echo ""
+echo ""
+echo "Done installing software!"
+echo ""
+echo ""
