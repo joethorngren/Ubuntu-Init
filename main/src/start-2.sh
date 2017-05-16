4acf2a3802ae8ec089bd08cf8d0d76baf9f0540f
Joe Thorngren <joethorngren@gmail.com>
2017-05-15 21:08:27 -0600

    Update zsh-completions..

--- main/src/start-2.sh
+++ main/src/start-2.sh
@@ -3,6 +3,13 @@
 rm -rf Templates Public Music Videos Examples Pictures examples.desktop
 mkdir ~/Apps ~/Android ~/Code_Complete/
 
+# zsh-completions
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-completions/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/zsh-completions.list"
+
+# Add zsh-completions repo key to apt
+wget -nv http://download.opensuse.org/repositories/shells:zsh-users:zsh-completions/xUbuntu_16.04/Release.key -O Release.key
+sudo apt-key add - < Release.key
+
 sudo add-apt-repository -y ppa:git-core/ppa
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-add-repository -y ppa:teejee2008/ppa
