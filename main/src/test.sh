d884ae3606052dc4fae06520da7f438067bd0c0b
Joe Thorngren <joethorngren@gmail.com>
2017-05-19 12:44:36 -0600

    NAS config stuff.

--- main/src/test.sh
+++ main/src/test.sh
@@ -2,6 +2,13 @@
 
 # NAS Shit...
 
-sudo apt install cifs-utils nfs-common
-sudo mount -t nfs 192.168.1.100:/volume1/Archives /mnt/Archives
+# TODO: Use whatever the username is...
 
+sudo apt install cifs-utils nfs-common
+sudo mkdir /media/dayfun/Archives
+sudo chown dayfun:dayfun /media/dayfun/Archives
+sudo mount -t nfs 192.168.1.100:/volume1/Archives /media/dayfun/Archives
+
+# TODO: Figure out what to do with .desktop file + i3?
+# TODO: Update Icon + update to whatever username is...
+# TODO: Symbolic link to shared drives + folders in Home Directory?
