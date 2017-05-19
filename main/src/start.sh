1e79f505fc4516405487b144adf22ed53d744155
Joe Thorngren <joethorngren@gmail.com>
2017-05-19 15:44:29 -0600

    Install Ubuntu-Mate-Login + Add Brother-MFC Install files.

--- main/src/start.sh
+++ main/src/start.sh
@@ -201,9 +201,17 @@ echo ""
 sudo sh -c 'cat  deb http://debian.sur5r.net/i3/ xenial universe > /etc/apt/sources.list.d/sur5r-i3.list'
 
 sudo apt update -y
-sudo apt install -y -f i3 i3blocks i3status i3lock nitrogen compton
+sudo apt install -y -f i3 i3blocks i3status i3lock nitrogen compton ubuntu-mate-welcome
 
 sudo dpkg -i ./lib/res/deb/playerctl-0.5.0_amd64.deb
+
+# Kill Unity
+
+sudo apt-get autoremove --purge compiz compiz-gnome compiz-plugins-default libcompizconfig0
+sudo apt-get autoremove --purge unity unity-common unity-services libunity-core-6 libunity-misc4 appmenu-gtk appmenu-gtk3 appmenu-qt overlay-scrollbar activity-log-manager-control-center firefox-globalmenu thunderbird-globalmenu
+
+# the following command will disable the desktop (we won't need it with i3!)
+gsettings set org.gnome.desktop.background show-desktop-icons false
 
 reboot
 
