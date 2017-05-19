76c07f72fb7baba3ddba1913036dd289699f584a
Joe Thorngren <joethorngren@gmail.com>
2017-05-19 14:18:59 -0600

    Combined test-start-after-clean.sh + start.sh.

--- main/src/start.sh
+++ main/src/start.sh
@@ -1,6 +1,53 @@
 #!/bin/bash
 
-# sh test-start-after-clean.sh
+echo ""
+echo ""
+echo "***********"
+echo "Updating..."
+echo "***********"
+echo ""
+echo ""
+
+sudo apt-get update -y
+
+echo ""
+echo ""
+echo "Done updating!"
+echo ""
+echo ""
+
+echo ""
+echo ""
+echo "***********"
+echo "Upgrading..."
+echo "***********"
+echo ""
+echo ""
+
+sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
+
+echo ""
+echo ""
+echo "Done upgrading!"
+echo ""
+echo ""
+
+echo ""
+echo ""
+echo "***********"
+echo "Cleaning house..."
+echo "***********"
+echo ""
+echo ""
+
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
+
+echo ""
+echo ""
+echo "Done cleaning!"
+echo ""
+echo ""
 
 echo "***********"
 echo "Updating Nvidia Drivers..."
