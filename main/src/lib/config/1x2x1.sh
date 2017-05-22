1f38def664b017caaf21c8f5238654d631dfc63b
Joe Thorngren <joethorngren@gmail.com>
2017-05-22 00:49:15 -0600

    Make Monitor #1 Primary + adjust monitors #3 + #4 position.

--- main/src/lib/config/1x2x1.sh
+++ main/src/lib/config/1x2x1.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-1 --mode 1920x1080 --pos 4520x960 --rotate left --output HDMI-0 --primary --mode 1920x1080 --pos 0x960 --rotate right --output DP-3 --off --output DP-2 --mode 3440x1440 --pos 1080x0 --rotate normal --output DP-1 --off --output DP-0 --mode 3440x1440 --pos 1080x1440 --rotate normal
+xrandr --output DVI-D-0 --off --output HDMI-1 --mode 1920x1080 --pos 4520x808 --rotate left --output HDMI-0 --mode 1920x1080 --pos 0x808 --rotate right --output DP-3 --off --output DP-2 --mode 3440x1440 --pos 1080x0 --rotate normal --output DP-1 --off --output DP-0 --primary --mode 3440x1440 --pos 1080x1440 --rotate normal
