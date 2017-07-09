4c571785e4afecad00d343d18840e65dfcaefd30
Joe Thorngren <joethorngren@gmail.com>
2017-07-08 18:45:55 -0600

    Add basic CLI utility functions file, starting with functions to change git config.

---
+++ main/src/cli-utils.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function setPersonalGitCredentials() {
+    git config --global user.name = "Joe Thorngren"
+    git config --global user.email = "joethorngren@gmail.com"
+}
+
+function setTackGitCredentials() {
+    git config --global user.name = "Joe Thorngren"
+    git config --global user.email = "joe.thorngren@tackmobile.com"
+}
